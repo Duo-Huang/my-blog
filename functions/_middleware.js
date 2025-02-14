d2acbd91a867ace1847bae51ff4ebbeed18ac945
duo huang <duo.huang@thoughtworks.com>
2025-02-14 10:32:05 +0800

    add page function

---
+++ functions/_middleware.js
@@ -0,0 +1,18 @@
+export async function onRequest({ request, next }) {
+  const url = new URL(request.url);
+
+  // 如果请求目标是特定外部域名，去掉 Referer 头
+  if (url.hostname === "jsd.012700.xyz") { // 替换为你需要处理的域名
+    const newRequest = new Request(request, {
+      headers: {
+        ...Object.fromEntries(request.headers),
+        Referer: undefined, // 移除 Referer
+      }
+    });
+
+    return fetch(newRequest);
+  }
+
+  // 其他请求正常处理
+  return next();
+}
